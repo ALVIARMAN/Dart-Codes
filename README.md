9d95247c755a90f1bb452dc04276dc9cc0e62061
ALVIARMAN <alviarman009@gmail.com>
2025-08-02 00:46:03 +0600

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Dart-Codes
+Aurthor : Alvi Arman
---
+++ simple.dart
@@ -0,0 +1,19 @@
+void main(){
+    var list=[10,20,5,7,9,12];
+    print(list);
+
+    var x=5;
+    var a='Hello';
+    var b="world";
+    print(a+' '+b);
+    var check=true;
+    if(check==false){
+        print("false");
+    }else{
+        print('true');
+    }
+    //hash-map: key-value pair
+    var map={'name' : 'Alvi', 'age': 22, 'addr' : 'Dhaka'};
+    print(map);
+    
+}
